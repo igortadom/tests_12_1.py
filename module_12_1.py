286e860101b18310a62bfd069fdd1ca866426cfd
igortadom <tadom@yandex.ru>
2024-10-28 00:21:25 +0300

    Add files via upload

---
+++ module_12_1.py
@@ -0,0 +1,28 @@
+import runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        run1 = runner.Runner('Name-1')
+        for i in range(10):
+            run1.walk()
+        self.assertEqual(run1.distance, 50)
+
+    def test_run(self):
+        run1 = runner.Runner('Name-2')
+        for i in range(10):
+            run1.run()
+        self.assertEqual(run1.distance, 100)
+
+    def test_challenge(self):
+        run1 = runner.Runner('Name-3')
+        run2 = runner.Runner('Name-4')
+        for i in range(10):
+            run1.walk()
+            run2.run()
+
+        self.assertNotEqual(run1.distance, run2.distance)
+
+if __name__ == '__main__':
+    unittest.main()
